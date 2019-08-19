1ce7167ba44b46ff68fca1fd79ba134443ef24f9
victor.petukhov <victor.petukhov@jetbrains.com>
2019-08-19 11:54:34 +0300

    Fail spec box tests if they have `unexpected behaviour` and passed

--- compiler/tests-common/tests/org/jetbrains/kotlin/codegen/CodegenTestCase.java
+++ compiler/tests-common/tests/org/jetbrains/kotlin/codegen/CodegenTestCase.java
@@ -837,8 +837,17 @@ public abstract class CodegenTestCase extends KtUsefulTestCase {
         callBoxMethodAndCheckResult(classLoader, aClass, method);
     }
 
-    protected void callBoxMethodAndCheckResult(URLClassLoader classLoader, Class<?> aClass, Method method)
+    private void callBoxMethodAndCheckResult(URLClassLoader classLoader, Class<?> aClass, Method method)
             throws IOException, IllegalAccessException, InvocationTargetException {
+        callBoxMethodAndCheckResult(classLoader, aClass, method, false);
+    }
+
+    protected void callBoxMethodAndCheckResult(
+            URLClassLoader classLoader,
+            Class<?> aClass,
+            Method method,
+            boolean unexpectedBehaviour
+    ) throws IOException, IllegalAccessException, InvocationTargetException {
         String result;
         if (BOX_IN_SEPARATE_PROCESS_PORT != null) {
             result = invokeBoxInSeparateProcess(classLoader, aClass);
@@ -858,7 +867,11 @@ public abstract class CodegenTestCase extends KtUsefulTestCase {
                 }
             }
         }
-        assertEquals("OK", result);
+        if (unexpectedBehaviour) {
+            assertNotSame("OK", result);
+        } else {
+            assertEquals("OK", result);
+        }
     }
 
     @NotNull
